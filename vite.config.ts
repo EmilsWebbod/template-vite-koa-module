177376fbcf5da303dda4f1e67f31cc9687382a0f
EmilsWebbod <emil@webbod.no>
2022-02-25 23:56:34 +0100

    Fix building

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-refresh';
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 2001,
     proxy: {
       '/api': {
         target: `http://localhost:2000`,
